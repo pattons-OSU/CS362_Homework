35ad65dc1f3f305848b45e40cb1f5d2da7b218ad
pattons-OSU <pattons@oregonstate.edu>
2021-05-01 13:06:06 -0700

    removing old commented code

--- Week_5/cube_volume.py
+++ Week_5/cube_volume.py
@@ -16,22 +16,3 @@ if __name__ == '__main__':
     user_input = user_input()
     print(f"The volume of a cube with side length of {user_input} is {cube_volume_calc(user_input)}.\n")
 
-"""
-##Trying to get the function to be mor robust with the addition of user defined
-##inputs and variable inputs for the side length.
-
-def cube_volume_calc():
-    print('')
-    side_length = input("Please enter the length of the side of the cube:")
-    side_length = int(side_length)
-    print('')
-    
-    cube_volume_result = side_length * 3
-
-    #print(f"The volume of a cube with a side length of {side_length} is {cube_volume}.")
-    #print('')
-
-    return cube_volume_result
-
-cube_volume_calc()
-"""
