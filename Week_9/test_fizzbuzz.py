7ae593d56cf4ef95f9f3020130e323927e14727b
pattons-OSU <pattons@oregonstate.edu>
2021-05-27 12:45:12 -0700

    implimented test to see if prgm ran

--- Week_9/test_fizzbuzz.py
+++ Week_9/test_fizzbuzz.py
@@ -11,10 +11,15 @@ import unittest
 import fizzbuzz
 
 class testCaseVolume(unittest.TestCase):
+    
     ## this test should pass, as the output is intended to be in a
     ## concantinated string format
     def test_datatype(self):
         self.assertTrue(type(fizzbuzz.output) == str)
+    
+    ##Test to see if program is executing based on user input
+    def test_did_run(self):
+        self.assertEqual(fizzbuzz.user_input(), 'y')
 
 
 if __name__ == '__main__':
