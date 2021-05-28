905cb9a6a85d6d0c8c6a65a956a4b2f84d851b26
pattons-OSU <pattons@oregonstate.edu>
2021-05-28 10:53:23 -0700

    implimenting yearcheck method that check input

--- Week_9/leapyear.py
+++ Week_9/leapyear.py
@@ -16,8 +16,24 @@ def user_input():
     return in_year
 
 
-
+def year_check(in_year):
+    ## A leap year is a year that is divisible (with no remainder) by 4 (mod 4)
+    ## If the year is evenly divisible by 100 it is not, unless it is simultaneously
+    ## divisible by 400 with no remainder.
+    if (in_year % 4) == 0:
+        if(in_year % 100) == 0:
+            if(in_year % 400) == 0:
+                print(in_year, "is a leap year.")
+            else:
+                print(in_year, "is not a leap year.")
+        else:
+            print(in_year, "is a leap year.")
+    else:
+        print(in_year, "is not a leap year.")
+    print("")
+    ## Return true here to verify that function is running
+    return True
 
 if __name__ == "__main__":
     in_year = user_input()
-    
+    year_check(in_year)
