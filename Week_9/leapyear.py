6572e6cdbd478daa5e5137d881ceb6430d1640cc
pattons-OSU <pattons@oregonstate.edu>
2021-05-28 10:41:08 -0700

    Creating leapyear.py that will pass initial test

---
+++ Week_9/leapyear.py
@@ -0,0 +1,23 @@
+#! python 3
+
+"""
+Simeon Patton
+CS362 - Oregon State Spring 2021
+Homework 7 - Question 2.2
+leapyear.py
+"""
+
+
+def user_input():
+    print("")
+    in_year = input('Please enter the year that you would like to check: ')
+    in_year = int(in_year)
+    print("")
+    return in_year
+
+
+
+
+if __name__ == "__main__":
+    in_year = user_input()
+    
